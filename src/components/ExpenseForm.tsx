df03d3847ead3bf31748fc322294d1a20a06ae12
Leodomi <Leodomi15@gmail.com>
2024-05-07 22:45:34 -0400

    creando modal

---
+++ src/components/ExpenseForm.tsx
@@ -0,0 +1,107 @@
+import { categories } from "../data/categories"
+import DatePicker from 'react-date-picker';
+import 'react-date-picker/dist/DatePicker.css';
+import 'react-calendar/dist/Calendar.css';
+import { ChangeEvent, useState } from "react";
+import { DraftExpense, Value } from '../types/index';
+
+
+export const ExpenseForm = () => {
+    const [expense, setExpense] = useState<DraftExpense>({
+        expenseName: '',
+        amount: 0,
+        category: '',
+        date: new Date()
+    })
+    const handleChange = (e: ChangeEvent<HTMLInputElement>| ChangeEvent<HTMLSelectElement>) => {
+        const { name, value } = e.target
+        const isAmountField=['amount'].includes(name)
+        setExpense({
+            ...expense,
+            [name]: isAmountField ? Number(value) : value
+        })
+    }
+    const handleChangeDate=(value:Value)=>{
+        setExpense({
+            ...expense,
+            date:value
+        })
+    }
+  return (
+    <form className="space-y-5">
+        <legend className="uppercase text-center text-2xl font-black border-b-4 
+        border-blue-600 py-2">
+            Nuevo gasto
+        </legend>
+
+        <div className="flex flex-col gap-2">
+            <label htmlFor="expenseName" className="text-xl">
+                Nombre del gasto
+            </label>
+            <input
+                type="text"
+                id="expenseName"
+                placeholder="Añade el nombre del gasto"
+                className="w-full p-2 border border-slate-200 bg-slate-200 rounded-lg"
+                name="expenseName"
+                value={expense.expenseName}
+                onChange={handleChange}
+            />
+        </div>
+
+        <div className="flex flex-col gap-2">
+            <label htmlFor="amount" className="text-xl">
+                Cantidad del gasto
+            </label>
+            <input
+                type="number"
+                id="amount"
+                placeholder="Añade la cantidad del gasto. Ej: 300"
+                className="w-full p-2 border border-slate-200 rounded-lg bg-slate-200"
+                name="amount"
+                value={expense.amount}
+                onChange={handleChange}
+            />
+        </div>
+
+        <div className="flex flex-col gap-2">
+            <label htmlFor="category" className="text-xl">
+                Categoria
+            </label>
+            <select
+                name="category"
+                id="category"
+                className="w-full p-2 border border-slate-200 rounded-lg bg-slate-200"
+                value={expense.category}
+                onChange={handleChange}
+            >
+                <option value="">-- Seleccione --</option>
+                {
+                    categories.map(category => (
+                        <option key={category.id} value={category.id}>
+                            {category.name}
+                        </option>
+                    ))
+                }
+             </select>   
+        </div>
+
+        <div className="flex flex-col gap-2">
+            <label htmlFor="amount" className="text-xl">
+                Fecha del gasto
+            </label>
+            <DatePicker
+                className="w-full p-2 border border-slate-200 rounded-lg bg-slate-200"
+                value={expense.date}
+                onChange={handleChangeDate}
+            />
+        </div>
+        <input 
+            type="submit" 
+            className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 
+            px-4 rounded cursor-pointer"
+            value="Anadir gasto"
+        />
+    </form>
+  )
+}
--- src/reducers/BudgetReducer.ts
+++ src/reducers/BudgetReducer.ts
@@ -1,19 +1,28 @@
 export type BudgetActions = 
-    {type: 'ADD_BUDGET', payload: {budget:number} } 
-
+    {type: 'ADD_BUDGET', payload: {budget:number} } |
+    {type: 'SHOW_MODAL'}|
+    {type: 'HIDE_MODAL'}
 
 export type BudgetState = {
     budget:number
+    modal:boolean
 }
 
 export const initialState: BudgetState = {
-    budget:0
+    budget:0,
+    modal:false
 }
 
 export const budgetReducer = (state: BudgetState, action: BudgetActions) => {
     
     if(action.type === 'ADD_BUDGET'){ 
         return {...state, budget: action.payload.budget}
+    }
+    if(action.type === 'SHOW_MODAL'){  
+        return {...state, modal: true}
+    }
+    if(action.type === 'HIDE_MODAL'){  
+        return {...state, modal: false}
     }
 
     return state;
